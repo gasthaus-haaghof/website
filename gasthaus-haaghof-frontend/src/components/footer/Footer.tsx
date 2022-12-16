389c3b3931de4d92a730a7e00a60ad5e7257a6ff
Jan Etschel <etschelj@gmail.com>
2022-12-16 11:44:06 +0100

    admin to footer

--- gasthaus-haaghof-frontend/src/components/footer/Footer.tsx
+++ gasthaus-haaghof-frontend/src/components/footer/Footer.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import {Typography} from "@mui/material";
+import { Typography } from "@mui/material";
 import styled from "@emotion/styled";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
-    return(
+    return (
         <StyledFooter className="footer">
             <Typography className="cr" zIndex={2}>Andreas und Katrin Kamberger, veröffentlicht 2022</Typography>
-            <Link to="/imprint" className="imprint" style={{ zIndex: 2}}>Impressum</Link>
+            <Link to="/imprint" className="imprint" style={{ zIndex: 2 }}>Impressum</Link>
+            <Link to="/site-admin" className="site-admin">Anmelden</Link>
         </StyledFooter>
     );
 };
@@ -16,7 +17,7 @@ const StyledFooter = styled.div`
     display: grid;
     grid-template-rows: auto, auto;
     grid-template-columns: auto auto;
-    grid-template-areas: "cr cr" "imprint imprint";
+    grid-template-areas: "cr cr" "imprint login";
     justify-items: center;
     gap: 0rem 1rem;
     
@@ -34,7 +35,7 @@ const StyledFooter = styled.div`
     
     > .imprint {
         grid-area: imprint;
-        justify-self: center;
+        justify-self: end;
     }
     
     > .site-admin {
